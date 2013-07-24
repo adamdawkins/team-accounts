9b767de5d07a4c356d3d97171f89567217ad2287
Adam Dawkins <adamdawkins@gmail.com>
2013-07-24 22:56:22 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamBudget::Application.config.secret_key_base = '493646feb3b074441c48baef5a181e2a43cae88ad6ee6811af69df554666346313d4014c55ab48420556c8f37cf0c8ed7aec811251d687cfcd73381600439da5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamBudget::Application.config.session_store :cookie_store, key: '_team-budget_session'
