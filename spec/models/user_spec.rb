fe81c23826d89fb0c9c5b7fca5bdbce299e80742
Adam Dawkins <adamdawkins@gmail.com>
2013-07-25 02:00:59 +0100

    Add email attribute validation to User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,12 @@ describe User do
   let(:user) { User.new(email: "adamdawkins@example.com") }
 
   describe "basic model" do
-      
+  
+    it { expect(user).to validate_presence_of :email }
+    it { expect(user).to validate_uniqueness_of :email }
+    it { expect(user).to allow_value('email@address.com').for(:email) }
+    it { expect(user).to_not allow_value('oeuoe').for(:email) }
+
     it "saves attributes" do
       result = user.save
       expect(result).to be true
