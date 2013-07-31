40bb82a016098b83527e87e456c7a8ae21c032f0
Adam Dawkins <adamdawkins@gmail.com>
2013-07-31 01:14:50 +0100

    Add user create and show actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save   
+      flash[:notice] = 'Success'
+      redirect_to @user
+    else
+      flash[:error] = 'An error has occured'
+    end
+  end
+
+  def show
+
+  end
+  
+  private 
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -7,24 +7,65 @@ describe "Users" do
       visit new_user_path
     end
 
-    it "renders the new user page" do
-      expect(page.status_code).to be (200)
+    describe "the page" do
+
+      it "renders the new user page" do
+        expect(page.status_code).to be (200)
+      end
+
+      it "prompts to create a new user" do
+        expect(page).to have_selector 'h1', text: 'Create user'
+      end
+
+      it "has an email input field" do
+        expect(page).to have_field 'user[email]'
+      end
+
+      it "has a password field" do
+        expect(page).to have_field 'user[password]'
+      end
+
+      it "has a password confirmation field" do
+        expect(page).to have_field 'user[password_confirmation]'
+      end
+
+      it "has a create user button" do
+        expect(page).to have_button 'Create User'
+      end
+
     end
 
-    it "prompts to create a new user" do
-      expect(page).to have_selector 'h1', text: 'Create user'
+    context "submit form with valid data" do
+      before(:each) do 
+        fill_in 'user[email]', with: 'john.smith@example.com'
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+        click_on 'Create User'
+      end
+
+      it  "returns a success message" do
+        expect(page).to have_content "Success"
+      end
+
+      it "creates a user record in the database" do
+        expect(User.where(email: 'john.smith@example.com').length).to eq(1)
+      end
+
     end
 
-    it "has an email input field" do
-      expect(page).to have_field 'user[email]'
-    end
+    context "submit form with non-matching passwords" do
+      it "displays an error" do
+        pending
+      end
 
-    it "has a password field" do
-      expect(page).to have_field 'user[password]'
-    end
+      it "does not save a user record in the database" do
+        pending
+      end
 
-    it "has a password confirmation field" do
-      expect(page).to have_field 'user[password_confirmation]'
+      it "remains on the new user page" do
+        pending
+      end
+
     end
 
   end
