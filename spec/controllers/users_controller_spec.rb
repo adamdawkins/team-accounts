e37b20c39759407953e71030f2a50e94c95c5e00
Adam Dawkins <adamdawkins@gmail.com>
2013-08-03 11:50:18 +0100

    Add users controller spec.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe UsersController do
-  pending "add some tests for existing actions"
+
+  describe "GET #new" do
+    before { get :new }
+    it "has a 200 status code" do
+      expect(response.status).to eq(200)
+    end
+
+    it "assigns @user to a new User object" do
+      expect(assigns[:user].class.name).to eq "User"
+    end 
+
+    it "renders the new template" do
+      expect(response).to render_template "new" 
+    end
+  end
+
+  describe "POST #create" do 
+    
+    context "with valid attributes" do
+      it "creates a new user" do 
+        expect {
+          post :create, user: {email: "adamdawkins@gmail.com", password: "password", password_confirmation: "password"}
+        }.to change(User, :count).by(1)
+      end
+
+      it "re-directs to the new user" do
+          post :create, user: {email: "adamdawkins@gmail.com", password: "password", password_confirmation: "password"}
+          expect(response).to redirect_to(User.last)
+      end
+    end
+    
+  end
+
 end
