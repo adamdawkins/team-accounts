d1572b8610f3238306873f261c3eb43908682f86
Adam Dawkins <adamdawkins@gmail.com>
2014-01-14 16:42:50 +0000

    Seed db with adamdawkins@gmail.com user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create at least one user to start with
+User.create({
+  email: "adamdawkins@gmail.com",
+  password:"password",
+  password_confirmation: "password"
+})
