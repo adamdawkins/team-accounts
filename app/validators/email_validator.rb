a36869207262251e250866f9619bbe49996c88b6
Adam Dawkins <adamdawkins@gmail.com>
2014-02-04 19:36:24 +0000

    Add Reek ignore comment to email validator

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,4 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
+  # Not sure how to avoid use of :reek:ControlParameter here
+  # It's copied from somewhere else, and does it's job
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
       record.errors.add attribute, (options[:message] || 'is not an email')
