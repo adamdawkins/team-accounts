37a218e2c895ce7b3f9e1ffca85673c73e6c385b
Adam Dawkins <adamdawkins@gmail.com>
2014-02-24 23:29:18 +0000

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20140222140654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "areas", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "balances", force: true do |t|
     t.date     "date"
     t.decimal  "amount"
